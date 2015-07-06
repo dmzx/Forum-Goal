44cec4e4434b1e9a9151c5d571851b9b84b6eb05
dmzx <info@dmzx-web.net>
2015-07-06 14:59:40 +0200

    Code update

--- event/acp_listener.php
+++ event/acp_listener.php
@@ -33,21 +33,19 @@ class acp_listener implements EventSubscriberInterface
 
 			// Define config vars
 			$config_vars = array(
-						'legend4'						=> 'FORUM_GOALS_SETTINGS',
-						'forum_goals_display_top'		=> array('lang' => 'FGDT_ENABLE',	'validate' => 'bool',	'type' => 'radio:yes_no', 'explain' => false),
-						'forum_goals_display_bottom'	=> array('lang' => 'FGDB_ENABLE',	'validate' => 'bool',	'type' => 'radio:yes_no', 'explain' => false),
-						'forum_goals_posts_enable'		=> array('lang' => 'FGP_ENABLE',	'validate' => 'bool',	'type' => 'radio:yes_no', 'explain' => false),
-						'forum_goals_posts'				=> array('lang' => 'FGP_NUMBER',	'validate' => 'int',		'type' => 'text:15:8', 'explain' => false),
-						'forum_goals_topics_enable'		=> array('lang' => 'FGT_ENABLE',	'validate' => 'bool',	'type' => 'radio:yes_no', 'explain' => false),
-						'forum_goals_topics'			=> array('lang' => 'FGT_NUMBER',	'validate' => 'int',		'type' => 'text:15:8', 'explain' => false),
-						'forum_goals_users_enable'		=> array('lang' => 'FUG_ENABLE',	'validate' => 'bool',	'type' => 'radio:yes_no', 'explain' => false),
-						'forum_goals_users'				=> array('lang' => 'FUG_NUMBER',	'validate' => 'int',		'type' => 'text:15:8', 'explain' => false),
-						'forum_goals_files_enable'		=> array('lang' => 'FFG_ENABLE',	'validate' => 'bool',	'type' => 'radio:yes_no', 'explain' => false),
-						'forum_goals_files'				=> array('lang' => 'FFG_NUMBER',	'validate' => 'int',		'type' => 'text:15:8', 'explain' => false),
-						'forum_goals_views_enable'		=> array('lang' => 'FTVG_ENABLE',	'validate' => 'bool',	'type' => 'radio:yes_no', 'explain' => false),
-						'forum_goals_views'				=> array('lang' => 'FTVG_NUMBER',	'validate' => 'int',		'type' => 'text:15:8', 'explain' => false),
-					//	'forum_goals_gallery_enable'	=> array('lang' => 'FGG_ENABLE',	'validate' => 'bool',	'type' => 'radio:yes_no', 'explain' => false),
-					//	'forum_goals_gallery'			=> array('lang' => 'FGG_NUMBER',	'validate' => 'int',		'type' => 'text:15:8', 'explain' => false),
+				'legend4'						=> 'FORUM_GOALS_SETTINGS',
+				'forum_goals_display_top'		=> array('lang' => 'FGDT_ENABLE',	'validate' => 'bool',	'type' => 'radio:yes_no', 'explain' => false),
+				'forum_goals_display_bottom'	=> array('lang' => 'FGDB_ENABLE',	'validate' => 'bool',	'type' => 'radio:yes_no', 'explain' => false),
+				'forum_goals_posts_enable'		=> array('lang' => 'FGP_ENABLE',	'validate' => 'bool',	'type' => 'radio:yes_no', 'explain' => false),
+				'forum_goals_posts'				=> array('lang' => 'FGP_NUMBER',	'validate' => 'int',		'type' => 'text:15:8', 'explain' => false),
+				'forum_goals_topics_enable'		=> array('lang' => 'FGT_ENABLE',	'validate' => 'bool',	'type' => 'radio:yes_no', 'explain' => false),
+				'forum_goals_topics'			=> array('lang' => 'FGT_NUMBER',	'validate' => 'int',		'type' => 'text:15:8', 'explain' => false),
+				'forum_goals_users_enable'		=> array('lang' => 'FUG_ENABLE',	'validate' => 'bool',	'type' => 'radio:yes_no', 'explain' => false),
+				'forum_goals_users'				=> array('lang' => 'FUG_NUMBER',	'validate' => 'int',		'type' => 'text:15:8', 'explain' => false),
+				'forum_goals_files_enable'		=> array('lang' => 'FFG_ENABLE',	'validate' => 'bool',	'type' => 'radio:yes_no', 'explain' => false),
+				'forum_goals_files'				=> array('lang' => 'FFG_NUMBER',	'validate' => 'int',		'type' => 'text:15:8', 'explain' => false),
+				'forum_goals_views_enable'		=> array('lang' => 'FTVG_ENABLE',	'validate' => 'bool',	'type' => 'radio:yes_no', 'explain' => false),
+				'forum_goals_views'				=> array('lang' => 'FTVG_NUMBER',	'validate' => 'int',		'type' => 'text:15:8', 'explain' => false),
 			);
 
 			$display_vars['vars'] = phpbb_insert_config_array($display_vars['vars'], $config_vars, array('after' => 'load_cpf_viewtopic'));
--- event/listener.php
+++ event/listener.php
@@ -46,10 +46,10 @@ class listener implements EventSubscriberInterface
 			$forum_goals_posts_percent = ($total_posts * 100) / $this->config['forum_goals_posts'];
 			$forum_goals_posts_rest = $this->config['forum_goals_posts'] - $total_posts;
 			$this->template->assign_vars(array(
-			'FPG_ENABLE'						=> $this->config['forum_goals_posts_enable'],
-			'FORUM_POST_GOAL'				=> number_format($this->config['forum_goals_posts']),
-			'FORUM_POST_PERCENT'				=> round($forum_goals_posts_percent),
-			'FORUM_POST_REST'				=> number_format($forum_goals_posts_rest),
+				'FPG_ENABLE'						=> $this->config['forum_goals_posts_enable'],
+				'FORUM_POST_GOAL'				=> number_format($this->config['forum_goals_posts']),
+				'FORUM_POST_PERCENT'				=> round($forum_goals_posts_percent),
+				'FORUM_POST_REST'				=> number_format($forum_goals_posts_rest),
 			 ));
 		}
 
@@ -59,10 +59,10 @@ class listener implements EventSubscriberInterface
 			$forum_goals_topics_percent = ($total_topics * 100) / $this->config['forum_goals_topics'];
 			$forum_goals_topics_rest = $this->config['forum_goals_topics'] - $total_topics;
 			$this->template->assign_vars(array(
-			'FTG_ENABLE'						=> $this->config['forum_goals_topics_enable'],
-			'FORUM_TOPIC_GOAL'				=> number_format($this->config['forum_goals_topics']),
-			'FORUM_TOPIC_PERCENT'			=> round($forum_goals_topics_percent),
-			'FORUM_TOPIC_REST'				=> number_format($forum_goals_topics_rest),
+				'FTG_ENABLE'						=> $this->config['forum_goals_topics_enable'],
+				'FORUM_TOPIC_GOAL'				=> number_format($this->config['forum_goals_topics']),
+				'FORUM_TOPIC_PERCENT'			=> round($forum_goals_topics_percent),
+				'FORUM_TOPIC_REST'				=> number_format($forum_goals_topics_rest),
 			));
 		}
 
@@ -72,10 +72,10 @@ class listener implements EventSubscriberInterface
 			$forum_goals_users_percent = ($total_users * 100) / $this->config['forum_goals_users'];
 			$forum_goals_users_rest = $this->config['forum_goals_users'] - $total_users;
 			$this->template->assign_vars(array(
-			'FUG_ENABLE'						=> $this->config['forum_goals_users_enable'],
-			'FORUM_USER_GOAL'				=> number_format($this->config['forum_goals_users']),
-			'FORUM_USER_PERCENT'				=> round($forum_goals_users_percent),
-			'FORUM_USER_REST'				=> number_format($forum_goals_users_rest),
+				'FUG_ENABLE'						=> $this->config['forum_goals_users_enable'],
+				'FORUM_USER_GOAL'				=> number_format($this->config['forum_goals_users']),
+				'FORUM_USER_PERCENT'				=> round($forum_goals_users_percent),
+				'FORUM_USER_REST'				=> number_format($forum_goals_users_rest),
 			));
 		}
 
@@ -86,11 +86,11 @@ class listener implements EventSubscriberInterface
 			$forum_goals_files_percent = ($total_files * 100) / $this->config['forum_goals_files'];
 			$forum_goals_files_rest = $this->config['forum_goals_files'] - $total_files;
 			$this->template->assign_vars(array(
-			'FFG_ENABLE'						=> $this->config['forum_goals_files_enable'],
-			'TOTAL_FILES'					=> sprintf($this->user->lang[$l_total_file_s], number_format($total_files)),
-			'FORUM_FILES_GOAL'				=> number_format($this->config['forum_goals_files']),
-			'FORUM_FILES_PERCENT'			=> round($forum_goals_files_percent),
-			'FORUM_FILES_REST'				=> number_format($forum_goals_files_rest),
+				'FFG_ENABLE'						=> $this->config['forum_goals_files_enable'],
+				'TOTAL_FILES'					=> sprintf($this->user->lang[$l_total_file_s], number_format($total_files)),
+				'FORUM_FILES_GOAL'				=> number_format($this->config['forum_goals_files']),
+				'FORUM_FILES_PERCENT'			=> round($forum_goals_files_percent),
+				'FORUM_FILES_REST'				=> number_format($forum_goals_files_rest),
 			));
 		}
 
@@ -99,10 +99,10 @@ class listener implements EventSubscriberInterface
 			$forum_goals_gallery_percent = (phpbb_gallery_config::get('num_images') * 100) / $this->config['forum_goals_gallery'];
 			$forum_goals_gallery_rest = $this->config['forum_goals_gallery'] - phpbb_gallery_config::get('num_images');
 			$this->template->assign_vars(array(
-			'FGG_ENABLE'						=> $this->config['forum_goals_gallery_enable'],
-			'FORUM_GALLERY_GOAL'				=> number_format($this->config['forum_goals_gallery']),
-			'FORUM_GALLERY_PERCENT'			=> round($forum_goals_gallery_percent),
-			'FORUM_GALLERY_REST'				=> number_format($forum_goals_gallery_rest),
+				'FGG_ENABLE'						=> $this->config['forum_goals_gallery_enable'],
+				'FORUM_GALLERY_GOAL'				=> number_format($this->config['forum_goals_gallery']),
+				'FORUM_GALLERY_PERCENT'			=> round($forum_goals_gallery_percent),
+				'FORUM_GALLERY_REST'				=> number_format($forum_goals_gallery_rest),
 			));
 		}
 
@@ -117,11 +117,11 @@ class listener implements EventSubscriberInterface
 			$forum_goals_views_percent = ($total_views * 100) / $this->config['forum_goals_views'];
 			$forum_goals_views_rest = $this->config['forum_goals_views'] - $total_views;
 			$this->template->assign_vars(array(
-			'FTVG_ENABLE'					=> $this->config['forum_goals_views_enable'],
-			'TOTAL_VIEWS'					=> $total_views,
-			'FORUM_VIEW_GOAL'				=> number_format($this->config['forum_goals_views']),
-			'FORUM_VIEW_PERCENT'			 => round($forum_goals_views_percent),
-			'FORUM_VIEW_REST'				=> number_format($forum_goals_views_rest),
+				'FTVG_ENABLE'					=> $this->config['forum_goals_views_enable'],
+				'TOTAL_VIEWS'					=> $total_views,
+				'FORUM_VIEW_GOAL'				=> number_format($this->config['forum_goals_views']),
+				'FORUM_VIEW_PERCENT'			 => round($forum_goals_views_percent),
+				'FORUM_VIEW_REST'				=> number_format($forum_goals_views_rest),
 			));
 		}
 
@@ -130,14 +130,14 @@ class listener implements EventSubscriberInterface
 			$donation_goal_number = ($this->config['donation_achievement'] * 100) / $this->config['donation_goal'];
 			$donation_goal_rest = $this->config['donation_goal'] - $this->config['donation_achievement'];
 			$this->template->assign_vars(array(
-			'DONATION_GOAL_NUMBER'			=> number_format($donation_goal_number),
-			'DONATION_GOAL_REST'				=> number_format($donation_goal_rest),
+				'DONATION_GOAL_NUMBER'			=> number_format($donation_goal_number),
+				'DONATION_GOAL_REST'				=> number_format($donation_goal_rest),
 			));
 		}
 
 		$this->template->assign_vars(array(
 			'FORUM_GOAL_TOP'	=> (!empty($this->config['forum_goals_display_top'])) ? true : false,
 			'FORUM_GOAL_BOTTOM'	=> (!empty($this->config['forum_goals_display_bottom'])) ? true : false,
-			));
+		));
 	}
 }
